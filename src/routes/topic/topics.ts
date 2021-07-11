025b847c40a56d98e8708d016056216c5c699d05
BreMea <63671187+BreMea@users.noreply.github.com>
2021-07-10 20:29:52 -0400

    fix func name

--- src/routes/topic/topics.ts
+++ src/routes/topic/topics.ts
@@ -3,8 +3,8 @@ import petitio from "petitio";
 
 export class TopicManager {
 
-    public async getPosts(topicId: Number): Promise<Array<FourmTopic>> {
-        return ((await petitio(`hhttps://scratchdb.lefty.one/v3/forum/topic/info/${topicId}`)).json());
+    public async getTopic(topicId: Number): Promise<Array<FourmTopic>> {
+        return (await petitio(`hhttps://scratchdb.lefty.one/v3/forum/topic/info/${topicId}`).json());
     }
 
 }
