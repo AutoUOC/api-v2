df19408766df50910db62d6fad3cee6b6bad8bdb
BreMea <63671187+BreMea@users.noreply.github.com>
2021-07-10 20:18:22 -0400

    switch to petitio

--- src/routes/post/posts.ts
+++ src/routes/post/posts.ts
@@ -1,9 +1,10 @@
 import { FourmTopic } from '../typings/topic.js';
+import petitio from "petitio";
 
 export class PostManager {
 
     public async getPosts(topicId: Number, pageNum: Number): Promise<Array<FourmTopic>> {
-        return ((await fetch(`https://scratchdb.lefty.one/v3/forum/topic/posts/${topicId}/${pageNum}`)).json());
+        return ((await petitio(`https://scratchdb.lefty.one/v3/forum/topic/posts/${topicId}/${pageNum}`)).json());
     }
 
 }
--- src/routes/topic/topics.ts
+++ src/routes/topic/topics.ts
@@ -1,9 +1,10 @@
 import { FourmTopic } from '../typings/topic.js';
+import petitio from "petitio";
 
 export class TopicManager {
 
     public async getPosts(topicId: Number): Promise<Array<FourmTopic>> {
-        return ((await fetch(`hhttps://scratchdb.lefty.one/v3/forum/topic/info/${topicId}`)).json());
+        return ((await petitio(`hhttps://scratchdb.lefty.one/v3/forum/topic/info/${topicId}`)).json());
     }
 
 }
